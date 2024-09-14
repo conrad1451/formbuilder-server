f35bb1c4d0716c3e747868fb93d78673195763c8
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-09-13 23:55:47 -0400

    fix: removed duplicate user entries
    Fixed the error of duplicate entries by generating random usernames, emails, and passwords for each user created.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// source: 
+// [1]https://www.programiz.com/javascript/examples/generate-random-strings 
+
 require('dotenv').config();
 const User = require("./models/Users");
 
@@ -41,11 +44,24 @@ db.once('open', () => {
 
 
 app.get('/mytestpage', async (req, res)=>{
+    // const nonhashedPass = "password"; // CHQ: for testing in case the hash causes issues
     const hashedPass = await bcrypt.hash("password", 10);   
 
+    const randomUsername = Math.random().toString(36).substring(2,7); //[1]
+    // const randomEmail = randomUsername + "@gmail.com";
+    const randomEmail = String(randomUsername + "@gmail.com");
+
+    const randomPass = await bcrypt.hash((randomUsername+"@passphrase"), 10);  
+
+    // const randomEmail = (Math.random().toString(36).substring(2,7);
+
     const user = new User({
-        email: 'testuser@gmail.com',
-        password: hashedPass,
+        username: randomUsername,
+        email: randomEmail,
+        // username: 'myusername',
+        // email: 'testuser@gmail.com',
+        password: randomPass
+        // password: hashedPass,
         // password: "testpass",
     });
 
@@ -55,9 +71,14 @@ app.get('/mytestpage', async (req, res)=>{
     res.send("Hello");
 })
 
-app.get('/users', (req, res)=>{
+app.get('/users', async (req, res)=>{
+
+    // app.get('/users', (req, res)=>{
+    // CHQ: This is hard coded. might cause issues 
     const myUser = User.findOne({email: 'testuser@gmail.com'});
-    res.send(JSON.stringify(myUser));
+    // FIXME: terminal yelled "TypeError: Converting circular structure to JSON" stackoverflow" 
+    // res.send(JSON.stringify(myUser)); 
+    // Let's try returning just the email
     console.log(myUser);
     res.send("Success")
 })
