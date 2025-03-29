476a2c1384c18c2485f6e4b0f1204ed55b3c6c15
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-03-29 16:07:35 -0400

    chore: no more nodemodules in repo

---
+++ models/BasicUsers.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email:{
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String, 
+        required: true,
+    }   
+})
+//Export the model
+module.exports = mongoose.model("User", userSchema);
